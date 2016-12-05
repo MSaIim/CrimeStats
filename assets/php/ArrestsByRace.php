1493adc918e03d287b1a14561335c42665446e99
Mohammad Salim <mfsalim27@gmail.com>
2016-12-05 17:23:26 -0500

    Make ArrestsByRace and RacePopulation more generic

--- assets/php/ArrestsByRace.php
+++ assets/php/ArrestsByRace.php
@@ -14,11 +14,21 @@
 		private $arrests_compare_asian;
 		private $arrests_compare_pacific;
 
+		// Binding array
+		private $fields;
+
+		// Columns and clauses
+		private $columns;
+		private $clauses;
+
 		// Intialize all the variables
 		function __construct()
 		{
 			$this->query = "";
 			$this->table = "ArrestsByRace";
+			$this->fields = array();
+			$this->columns = array();
+			$this->clauses = array();
 
 			$this->arrests_offense = $_POST['arrests-offense'];
 			$this->arrests_race = $_POST['arrests-race'];
@@ -29,57 +39,130 @@
 			$this->arrests_compare_pacific = (isset($_POST['arrests-compare-pacific'])) ? $_POST['arrests-compare-pacific'] : "";
 		}
 
+		// Get the bindings for query so PDO can use it
+		public function getBindings()
+		{
+			return $this->fields;
+		}
+
+		// Remove last element from column if no results found
+		public function tryNewQuery()
+		{
+			// Pop a race and build new query
+			array_pop($this->columns);
+			array_pop($this->columns);
+			$this->build();
+
+			// Return new query
+			return $this->query;
+		}
+
 		// Return the query for the arrests by race table
 		public function getQuery()
 		{
 			// Offense selected and maybe race
 			if($this->arrests_offense != "")
 			{
-				$this->query = "SELECT * FROM " . $this->table . " WHERE `Offense Charged` = :arrests_offense";
+				// Set the fields
+				$this->setFields();
 
-				if($this->getRaceQuery())
-					$this->query .= " FROM " . $this->table . " WHERE `Offense Charged` = :arrests_offense AND " . $this->arrests_race . " >= 0";
+				// Check if race was chosen
+				if(sizeof($this->columns) == 3)
+				{
+					$this->columns = array();
+					array_push($this->columns, "*");
+				}
+
+				// Build the query
+				$this->build();
 			}
 			// Offense not selected and maybe race
-			else if($this->arrests_offense == "")
+			else if($this->arrests_offense == "" && $this->arrests_race != "")
 			{
-				if($this->getRaceQuery())
-					$this->query .= " FROM " . $this->table . " WHERE " . $this->arrests_race . " >= 0";
+				$this->setFields();
+				$this->build();
 			}
 
 			return $this->query;
 		}
 
-		// Get the bindings for query so PDO can use it
-		public function getBindings()
+		// Build the query from the arrays
+		private function build()
 		{
-			return array(":arrests_offense"=>$this->arrests_offense);
+			// Start query
+			$this->query = "SELECT ";
+
+			// Add all the columns
+			for($i = 0; $i < sizeof($this->columns); $i++)
+			{
+				$this->query .= $this->columns[$i] . ",";
+			}
+
+			// Remove last comma and add the table name
+			$this->query = substr($this->query, 0, strlen($this->query) - 1) . " FROM " . $this->table;
+
+			// Add the clauses
+			for($i = 0; $i < sizeof($this->clauses); $i++)
+			{
+				$this->query .= $this->clauses[$i];
+			}
+		}
+
+		// Set the columns and clauses
+		private function setFields()
+		{
+			// Columns that will always be in query
+			array_push($this->columns, "`Offense Charged`");
+			array_push($this->columns, "Year");
+			array_push($this->columns, "Total");
+
+			// OFFENSE SELECTED
+			if($this->arrests_offense != "")
+			{
+				// Push clause and bind the field
+				array_push($this->clauses, " WHERE `Offense Charged` = :arrests_offense");
+				$this->fields[":arrests_offense"] = $this->arrests_offense;
+			}
+			// RACE SELECTED
+			if($this->arrests_race != "")
+			{
+				// Push column in
+				array_push($this->columns, $this->arrests_race);
+				array_push($this->columns, "`" . $this->arrests_race . " Percent`");
+
+				// Check if compare races are chosen
+				$this->getCompareRace();
+			}
 		}
 
 		// Get the query if Race is selected along with any comparisons
-		private function getRaceQuery()
+		private function getCompareRace()
 		{
-			// Boolean to see if race is selected
-			$haveRace = false;
-
-			if($this->arrests_race != "")
+			if(isset($_POST['arrests-compare-white']))
 			{
-				$haveRace = true;
-				$this->query = "SELECT `Offense Charged`, Year, Total, " . $this->arrests_race . ", `" . $this->arrests_race . " Percent`";
-
-				if(isset($_POST['arrests-compare-white']) && $this->arrests_compare_white != $this->arrests_race)
-					$this->query .= ", " . $this->arrests_compare_white . ", `" . $this->arrests_compare_white . " Percent`";
-				if(isset($_POST['arrests-compare-black']) && $this->arrests_compare_black != $this->arrests_race)
-					$this->query .= ", " . $this->arrests_compare_black . ", `" . $this->arrests_compare_black . " Percent`";
-				if(isset($_POST['arrests-compare-native']) && $this->arrests_compare_native != $this->arrests_race)
-					$this->query .= ", " . $this->arrests_compare_native . ", `" . $this->arrests_compare_native . " Percent`";
-				if(isset($_POST['arrests-compare-asian']) && $this->arrests_compare_asian != $this->arrests_race)
-					$this->query .= ", " . $this->arrests_compare_asian . ", `" . $this->arrests_compare_asian. " Percent`";
-				if(isset($_POST['arrests-compare-pacific']) && $this->arrests_compare_pacific != $this->arrests_race)
-					$this->query .= ", " . $this->arrests_compare_pacific . ", `" . $this->arrests_compare_pacific . " Percent`";
+				array_push($this->columns, $this->arrests_compare_white);
+				array_push($this->columns, "`" . $this->arrests_compare_white . " Percent`");
 			}
-
-			return $haveRace;
+			if(isset($_POST['arrests-compare-black']))
+			{
+				array_push($this->columns, $this->arrests_compare_black);
+				array_push($this->columns, "`" . $this->arrests_compare_black . " Percent`");
+			}
+			if(isset($_POST['arrests-compare-black']))
+			{
+				array_push($this->columns, $this->arrests_compare_native);
+				array_push($this->columns, "`" . $this->arrests_compare_native . " Percent`");
+			}
+			if(isset($_POST['arrests-compare-black']))
+			{
+				array_push($this->columns, $this->arrests_compare_asian);
+				array_push($this->columns, "`" . $this->arrests_compare_asian . " Percent`");
+			}
+			if(isset($_POST['arrests-compare-black']))
+			{
+				array_push($this->columns, $this->arrests_compare_pacific);
+				array_push($this->columns, "`" . $this->arrests_compare_pacific . " Percent`");
+			}
 		}
 	}
 ?>
--- assets/php/RacePopulation.php
+++ assets/php/RacePopulation.php
@@ -14,11 +14,17 @@
 		// Binding Array
 		private $fields;
 
+		// Columns and clauses
+		private $columns;
+		private $clauses;
+
 		function __construct()
 		{
 			$this->query = "";
 			$this->table = "RacePopulation";
 			$this->fields = array();
+			$this->columns = array();
+			$this->clauses = array();
 
 			$this->pop_state = $_POST['population-states'];
 			$this->pop_race = $_POST['population-race'];
@@ -26,91 +32,104 @@
 			$this->pop_percent = isset($_POST['population-percentage']);
 		}
 
+		// Get the bindings for query so PDO can use it
 		public function getBindings()
 		{
 			return $this->fields;
 		}
 
+		// Remove last element from column if no results found
+		public function tryNewQuery()
+		{
+			// Pop a select statement and build new query
+			array_pop($this->clauses);
+			$this->build();
+
+			// Return new query
+			return $this->query;
+		}
+
+		// Return the query for the race population table
 		public function getQuery()
 		{
-			$hasRace = false;
-			$query = "";
+			// Set the fields chosen
+			$this->setFields();
 
-			// State selected and maybe race
-			if($this->pop_state != "")
+			// Check if race was chosen
+			if(sizeof($this->columns) == 3)
 			{
-				// Bind the state
-				$this->fields[":state"] = $this->pop_state;
-
-				// Set the columns
-				$columns = "state,year,total";
-				$query = "SELECT " . $columns . " FROM (";
-
-				// Check if race selected
-				if($this->pop_race != "" && $this->pop_race != "All")
-				{
-					// Race chosen
-					$hasRace = true;
-
-					// Set the columns
-					if($this->pop_percent)
-						$columns .= "," . "(FORMAT ((" . $this->pop_race . "/total * 100), 1)) AS '" . $this->pop_race . " Percentage Of Total Population'";
-					else
-						$columns .= "," . $this->pop_race;
-
-					$query = "SELECT " . $columns . " FROM (";
-				}
-
-				// See if race was selected
-				if(!$hasRace)
-					$query = "SELECT * FROM (";
-
-				// Create query
-				if($this->pop_state != "All")
-				{
-					if($hasRace)
-						$this->query .= "SELECT * FROM " . $this->table . " WHERE state = :state AND " . $this->pop_race . " >= 0";
-					else
-						$this->query .= "SELECT * FROM " . $this->table . " WHERE state = :state";
-
-					// Check if compare state is selected
-					if($this->pop_state_compare != "")
-					{
-						// Bind the state
-						$this->fields[":state_compare"] = $this->pop_state_compare;
-						$this->query .= " UNION SELECT * FROM " . $this->table . " WHERE state = :state_compare";
-
-						if($hasRace)
-							$this->query .= " AND " . $this->pop_race . " >= 0";
-					}
-
-					// End query
-					$this->query = $query . $this->query . ") AS R ORDER BY state";
-				}
-				else
-				{
-					if($hasRace)
-						$this->query = "SELECT " . $columns . " FROM " . $this->table . " WHERE " . $this->pop_race . " >= 0";
-					else
-						$this->query = "SELECT * FROM " . $this->table;
-				}
+				$this->columns = array();
+				array_push($this->columns, "*");
 			}
 
-			// State not selected and race selected
-			else if($this->pop_race != "")
-			{
-				// Default race 
-				$raceCol = $this->pop_race;
-
-				// Race as percentage
-				if($this->pop_percent)
-					$raceCol = "(FORMAT ((" . $this->pop_race . "/total * 100), 1)) AS '" . $this->pop_race . " Percentage Of Total Population'";
-
-				$this->query = "SELECT state,year,total," . $raceCol . " FROM " . $this->table . " WHERE " . $this->pop_race . " >= 0";
-			}
-
+			// Build the query
+			$this->build();
+			
 			// Return it
+			echo $this->query;
 			return $this->query;
+		}
+
+		// Build the query
+		private function build()
+		{
+			// Start query
+			$this->query = "SELECT ";
+
+			// Add all the columns
+			for($i = 0; $i < sizeof($this->columns); $i++)
+			{
+				$this->query .= $this->columns[$i] . ",";
+			}
+
+			// Remove comma at end and add FROM clause
+			$this->query = substr($this->query, 0, strlen($this->query) - 1) . " FROM (";
+
+			// Add all the clauses
+			for($i = 0; $i < sizeof($this->clauses); $i++)
+			{
+				$this->query .= $this->clauses[$i] . " UNION ";
+			}
+
+			// End query
+			if(sizeof($this->clauses) > 0)
+				$this->query = substr($this->query, 0, strlen($this->query) - 7) . ") AS R ORDER BY state";
+			else
+				$this->query = substr($this->query, 0, strlen($this->query) - 1) . $this->table;
+		}
+
+		// Set the columns and clauses
+		private function setFields()
+		{
+			// Default columns
+			array_push($this->columns, "year");
+			array_push($this->columns, "state");
+			array_push($this->columns, "total");
+
+			// STATE SELECTED AND MAYBE COMPARISON STATE
+			if($this->pop_state != "" && $this->pop_state != "All")
+			{
+				// Push clause and bind the field
+				array_push($this->clauses, "SELECT * FROM " . $this->table . " WHERE state = :pop_state");
+				$this->fields[":pop_state"] = $this->pop_state;
+
+				// Check if compare state is up
+				if($this->pop_state_compare != "")
+				{
+					// Push and bind
+					array_push($this->clauses, "SELECT * FROM " . $this->table . " WHERE state = :pop_state_compare");
+					$this->fields[":pop_state_compare"] = $this->pop_state_compare;
+				}
+			}
+			// RACE SELECTED
+			if($this->pop_race != "" && $this->pop_race != "All")
+			{
+				// Push column
+				if($this->pop_percent)
+					array_push($this->columns, "(FORMAT ((" . $this->pop_race . "/total * 100), 1)) AS '" . $this->pop_race . " Percentage Of Total Population'");
+				else
+					array_push($this->columns, $this->pop_race);
+			}
 		}
 	}
 ?>
